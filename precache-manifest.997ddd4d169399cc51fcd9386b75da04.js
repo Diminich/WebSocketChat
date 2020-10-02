5970a0b7dfb27942092af2c1d4351993dcf212ab
Diminich <hodarenokd@gmail.com>
2020-10-02 20:13:01 +0300

    Updates

---
+++ precache-manifest.997ddd4d169399cc51fcd9386b75da04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ca70d2021b25e74f991f7e247e66929",
+    "url": "/WebSocketChat/index.html"
+  },
+  {
+    "revision": "9fea446300f2cb306a40",
+    "url": "/WebSocketChat/static/css/main.37a91597.chunk.css"
+  },
+  {
+    "revision": "5f8d0ed15a948aad689e",
+    "url": "/WebSocketChat/static/js/2.48503413.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/WebSocketChat/static/js/2.48503413.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fea446300f2cb306a40",
+    "url": "/WebSocketChat/static/js/main.7c5f37d4.chunk.js"
+  },
+  {
+    "revision": "c1003adb2673ba895139",
+    "url": "/WebSocketChat/static/js/runtime-main.7aeb4b5e.js"
+  }
+]);
